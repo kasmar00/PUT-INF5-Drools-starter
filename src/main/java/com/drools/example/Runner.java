4a8fe73b39aed173b21415b18c10239269b744c2
Marcin Kasznia <kasmar00@gmail.com>
2021-12-08 12:57:19 +0100

    pozar cleaned

--- src/main/java/com/drools/example/Runner.java
+++ src/main/java/com/drools/example/Runner.java
@@ -7,22 +7,19 @@ import org.drools.compiler.compiler.DroolsParserException;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-public class HelloWorldTest {
+public class Runner {
     public static void main(String[] args) throws DroolsParserException,
             IOException {
-        HelloWorldTest helloWorldTest = new HelloWorldTest();
-        helloWorldTest.executeHelloWorldRules();
+        Runner runner = new Runner();
+        runner.executeHelloWorldRules();
     }
     public void executeHelloWorldRules() throws IOException,
             DroolsParserException {
         KieServices ks = KieServices.Factory.get();
         BasicConfigurator.configure();
-        Logger.getLogger(HelloWorldTest.class).setLevel(Level.OFF);
+        Logger.getLogger(Runner.class).setLevel(Level.OFF);
         KieContainer kContainer = ks.getKieClasspathContainer();
         KieSession session = kContainer.newKieSession("ksession-rules");
-        HelloWorld helloWorld = new HelloWorld();
-        helloWorld.setPrintMessage("Hello World");
-        session.insert(helloWorld);
 //        session.insert(new Pomiar(1, 200));
         session.fireAllRules();
         session.insert(new Straz());
